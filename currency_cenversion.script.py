6111675017093aaa8720b8d04f3ff602e82be134
MykolaWauer <51316424+MykolaWauer@users.noreply.github.com>
2023-11-22 11:51:46 +0100

    Update currency_cenversion.script.py

--- currency_cenversion.script.py
+++ currency_cenversion.script.py
@@ -2,7 +2,7 @@ import pandas as pd
 from forex_python.converter import CurrencyRates
 
 # Load the Excel file
-file_path = r'C:\Users\wauer_m\Desktop\AdHocs\Sconto\Sconto_Umsatzwerte.xlsx'
+file_path = "path/to/your/excel/file.xlsx"
 df = pd.read_excel(file_path)
 
 # Define the currency conversion rate
@@ -12,7 +12,7 @@ conversion_rate = CurrencyRates().get_rate('CZK', 'EUR')
 df['Umsatz_Euro'] = df['Umsatz'] * conversion_rate
 
 # Save the result to a new Excel file
-output_path = r'C:\Users\wauer_m\Desktop\AdHocs\Sconto\Sconto_Umsatzwerte_Converted.xlsx'
+output_path = "path/to/your/output/file.xlsx"
 df.to_excel(output_path, index=False)
 
 print(f"Conversion completed. Results saved to {output_path}")
